8c35c27e5235824155d24ee8fbfd9a20a727ad7a
Gagan Janjua <gj1118@gmail.com>
2026-05-30 22:39:37 -0400

    updating wezterm theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,9 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-function scheme_for_appearance(appearance)
+local act = wezterm.action
+
+local function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
     -- return "dayfox"
     return "Catppuccin Mocha"
@@ -10,49 +12,70 @@ function scheme_for_appearance(appearance)
   end
 end
 
-config.font = wezterm.font(
-  -- { family = "Cascadia Code", weight = 'Regular' },
-  { family = "Agave", weight = 'Regular' }
-)
 
-config.keys = {
-  -- Split horizontally (left/right panes)
-  {
-    key = 'v',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
+config = {
+  use_fancy_tab_bar = false,
+  -- window_background_opacity = 0.94 ,
+  adjust_window_size_when_changing_font_size = true,
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 
-  -- Split vertically (top/bottom panes)
-  {
-    key = 'h',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  initial_cols = 130,
+  initial_rows = 45,
+  automatically_reload_config = true,
+  enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  window_decorations = "RESIZE",
+  font = wezterm.font("Liga Agave", { weight = "Regular" }),
+  font_size = 23,
+  background = {
+    {
+      source = {
+        File = "/Users/gjanjua/sandbox/wallpaper/termbackground.png",
+      },
+      width = "100%",
+      height = "100%",
+      opacity = 0.87,
+    }
   },
+  default_cursor_style = 'BlinkingBlock',
+  -- window_background_opacity = 0.9,
+  -- On macOS, this adds blur effect
+  macos_window_background_blur = 90,
 
-  {
-    key = 'Q',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.PaneSelect { mode = 'SwapWithActiveKeepFocus' },
+  keys = {
+    { key = "Enter", mods = "SUPER", action = act.ToggleFullScreen },
+    { key = "Enter", mods = "ALT",   action = act.DisableDefaultAssignment },
+    {
+      key = 'v',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+
+    -- Split vertically (top/bottom panes)
+    {
+      key = 'h',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+
+    {
+      key = 'Q',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.PaneSelect { mode = 'SwapWithActiveKeepFocus' },
+    },
+    -- navigate between panes
+    { key = 'LeftArrow',  mods = 'ALT',        action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'RightArrow', mods = 'ALT',        action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 'UpArrow',    mods = 'ALT',        action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'DownArrow',  mods = 'ALT',        action = wezterm.action.ActivatePaneDirection 'Down' },
+    -- resize panes:
+    { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Left', 5 } },
+    { key = 'RightArrow', mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Right', 5 } },
+    { key = 'UpArrow',    mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Up', 5 } },
+    { key = 'DownArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Down', 5 } },
+
   },
-  -- navigate between panes
-  { key = 'LeftArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
-  { key = 'RightArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
-  { key = 'UpArrow',    mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
-  { key = 'DownArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
-  -- resize panes:
-  { key = 'LeftArrow', mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Left', 5 } },
-  { key = 'RightArrow', mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Right', 5 } },
-  { key = 'UpArrow', mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Up', 5 } },
-  { key = 'DownArrow', mods = 'CTRL|SHIFT', action = wezterm.action.AdjustPaneSize { 'Down', 5 } },
 }
-
-config.use_fancy_tab_bar= false
-config.enable_tab_bar = false
-config.font_size=18
--- config.window_background_opacity = 0.94 
-config.adjust_window_size_when_changing_font_size = true
-config.hide_tab_bar_if_only_one_tab = true
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 return config
 
